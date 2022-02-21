4c4667b0f762115b0f9c30185f7385bbc8dfe63f
Random Guy <leekhoi5319990@gmail.com>
2022-02-21 13:48:45 +0700

    Show window

--- app/main.js
+++ app/main.js
@@ -1,1 +1,16 @@
-console.log('This is an Electron application!');
+const { app, BrowserWindow } = require('electron');
+
+let mainWindow = null; // so that the window stays after being mounted and not potentially be garbage collected (i.e. it'll stay until the user actually close it)
+
+// After the app is MOUNTED
+app.on('ready', function () {
+	mainWindow = new BrowserWindow({ show: false });
+	mainWindow.loadFile(`${__dirname}/index.html`);
+
+	// to get rid of the blank screen flash right before the HTML is mounted
+	mainWindow.once('ready-to-show', () => {
+		mainWindow.show();
+	});
+});
+
+console.log('It is.');
